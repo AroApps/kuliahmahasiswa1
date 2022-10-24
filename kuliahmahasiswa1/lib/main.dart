9c36bde3b203796fd978fa1b2fb92e5b6063f253
aliaaro <awwalia.21030@mhs.unesa.ac.id>
2022-10-25 00:30:46 +0700

    projek pertama

---
+++ kuliahmahasiswa1/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Mata Kuliah Mahasiswa Pendidikan Teknologi Informasi',
+      theme: ThemeData(
+        primarySwatch:Colors.blue,
+        primaryColor: Colors.white,
+        textTheme: TextTheme(bodyText2: TextStyle(color: Colors.white)),
+      ),
+      home: const Scaffold(),
+    );
+  }
+}
